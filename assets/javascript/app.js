a3ebb5737bdc82733c8275951dd43b09e8136cd2
Joewan Eiland <Joewan.Eiland@gmail.com>
2020-07-16 22:43:08 -0700

    Addded Api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,3 +30,66 @@ $(document).ready(function() {
                 
             }
             }); 
+            
+//Retrieve data using AJAX and API + BUTTON CLICK EVENT
+
+$(document.body).on('click', '#movieBtn' , function () { 
+
+    //makes sure the same gif doesnt get looped over and over
+    $('#gif-display').empty(); 
+
+
+    
+    //Stores all of the buttons with data-movie a
+    var movie = $(this).attr("data-movie") 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=HD8aqxdjXpLQMq0OBrTB0LWnEtvhgUZl&limit=10"; //SET &limit=10 at the end of the API Key
+
+        $.ajax({            
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response){
+            
+        console.log(response);
+
+        var movieResult = response.data;    //Creating movieResult variable to store the data from the response call
+    
+            for (var i= 0; i < movieResult.length ; i++) {  //Creating a loop for response data from GIPHY
+    
+            var movieDiv = $('<div>');  //Creating a new div and store the info inside movidDiv variable
+            var p = $('<p>');           //Creating a paragraph tag to display the rating
+
+            $(p).append('Rating:' + ' ' + movieResult[i].rating);
+        
+            var movieGif = $('<img class= "gif_click">');   //Creating <img> tag and store the gifs in movieGif variable
+
+            $(movieGif).attr("src", movieResult[i].images.fixed_height_still.url);          //Provide url attribute to the img fixed_height_still.url make the gif appear to be still
+            $(movieGif).attr({"data-animate" : movieResult[i].images.fixed_height.url});    //fixed_height.url is animated gif
+            $(movieGif).attr({'data-state': 'still'});                                      //Provide data-sate attribute to the img
+            $(movieGif).attr({"data-still": movieResult[i].images.fixed_height_still.url}); //Provide data-still attribute to the img. 
+
+
+            $(movieDiv).append(movieGif);       //Append the movieGif variable to movieDiv
+            $(movieDiv).append(p)               //Append p to movieDiv      
+
+            $('#gif-display').prepend(movieDiv); //Append movieDiv into DOM #gif-display 
+
+            }
+            }) 
+    }) 
+
+$(document.body).on('click' , '.gif_click' , function() {
+console.log('img')
+
+var state = $(this).attr("data-state");
+
+if (state === 'still') {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state" , "animate");
+
+} else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state" , "still");
+}
+
+});
